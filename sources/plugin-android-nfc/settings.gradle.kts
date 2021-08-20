92dbb74907dc9a8debecad903d38f2477e9cfd5c
Andrei Cristea <andrei.cristea019@gmail.com>
2021-08-20 17:12:56 +0200

    wip

---
+++ sources/plugin-android-nfc/settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+        jcenter()
+    }
+}
+rootProject.name = "plugin-android-nfc-example"
