b8dea2e596c07e666d104a7aa7ae86d09644edba
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2022-04-06 12:10:12 +0200

    feat: improve example code

--- Example_Plugin_PCSC/src/main/java/org/eclipse/keyple/plugin/pcsc/example/UseCase4_TransmitControl/Main_TransmitControl_Pcsc.java
+++ Example_Plugin_PCSC/src/main/java/org/eclipse/keyple/plugin/pcsc/example/UseCase4_TransmitControl/Main_TransmitControl_Pcsc.java
@@ -37,7 +37,8 @@ import org.slf4j.LoggerFactory;
  * setting parameters, controlling LEDs, a buzzer or any other proprietary function defined by the
  * reader manufacturer.
  *
- * <p>Here, we show its use to change the color of the RGB LEDs of a SpringCard "Puck One" reader.
+ * <p>Here, we show its use to change the color of the RGB LEDs and activate the buzzer of a
+ * SpringCard "Puck One" reader.
  *
  * <h2>Scenario</h2>
  *
@@ -61,6 +62,7 @@ public class Main_TransmitControl_Pcsc {
   private static final byte[] CMD_SET_LED_GREEN = HexUtil.toByteArray("581E000100");
   private static final byte[] CMD_SET_LED_BLUE = HexUtil.toByteArray("581E000001");
   private static final byte[] CMD_SET_LED_YELLOW = HexUtil.toByteArray("581E010100");
+  private static final byte[] CMD_BUZZER_200MS = HexUtil.toByteArray("589300C8");
   private static final Object waitForEnd = new Object();
 
   public static void main(String[] args) throws InterruptedException {
@@ -72,39 +74,56 @@ public class Main_TransmitControl_Pcsc {
     // return.
     Plugin plugin = smartCardService.registerPlugin(PcscPluginFactoryBuilder.builder().build());
 
-    // Get the first available reader (we assume that a SpringCard Puck One reader is connected)
-    Reader reader = plugin.getReaders().iterator().next();
+    // Get the contactless reader (we assume that a SpringCard Puck One reader is connected)
+    ObservableReader reader = null;
+    for (Reader r : plugin.getReaders()) {
+      if (r.getName().toLowerCase().contains("contactless")) {
+        reader = (ObservableReader) r;
+      }
+    }
+
+    if (reader == null) {
+      throw new IllegalStateException("Reader not found");
+    }
 
     PcscReader pcscReader = reader.getExtension(PcscReader.class);
 
     // Change the LED color to yellow when no card is connected
-    pcscReader.transmitControlCommand(pcscReader.getIoctlCcidEscapeCommandId(), CMD_SET_LED_YELLOW);
+    for (int i = 0; i < 3; i++) {
+      pcscReader.transmitControlCommand(
+          pcscReader.getIoctlCcidEscapeCommandId(), CMD_SET_LED_YELLOW);
+      Thread.sleep(200);
+      pcscReader.transmitControlCommand(pcscReader.getIoctlCcidEscapeCommandId(), CMD_SET_LED_BLUE);
+      Thread.sleep(200);
+    }
 
     // Get the generic card extension service
-    GenericExtensionService cardExtension = GenericExtensionService.getInstance();
+    GenericExtensionService genericExtensionService = GenericExtensionService.getInstance();
+
+    // check the extension
+    smartCardService.checkCardExtension(genericExtensionService);
 
     // Get the core card selection manager.
     CardSelectionManager cardSelectionManager = smartCardService.createCardSelectionManager();
 
     // Create a card selection using the generic card extension.
-    // Select the card and read the record 1 of the file ENVIRONMENT_AND_HOLDER
-    GenericCardSelection cardSelection = cardExtension.createCardSelection().filterByDfName(AID);
+    GenericCardSelection cardSelection =
+        genericExtensionService.createCardSelection().filterByDfName(AID);
 
     // Prepare the selection by adding the created selection to the card selection scenario.
     cardSelectionManager.prepareSelection(cardSelection);
 
     // Schedule the selection scenario, always notify card presence.
     cardSelectionManager.scheduleCardSelectionScenario(
-        (ObservableCardReader) reader,
+        reader,
         ObservableCardReader.DetectionMode.REPEATING,
         ObservableCardReader.NotificationMode.ALWAYS);
 
     CardObserver cardObserver = new CardObserver(pcscReader);
 
-    ((ObservableCardReader) reader).setReaderObservationExceptionHandler(cardObserver);
-    ((ObservableCardReader) reader).addObserver(cardObserver);
-    ((ObservableCardReader) reader)
-        .startCardDetection(ObservableCardReader.DetectionMode.REPEATING);
+    reader.setReaderObservationExceptionHandler(cardObserver);
+    reader.addObserver(cardObserver);
+    reader.startCardDetection(ObservableCardReader.DetectionMode.REPEATING);
 
     // Wait indefinitely. CTRL-C to exit.
     synchronized (waitForEnd) {
@@ -133,24 +152,40 @@ public class Main_TransmitControl_Pcsc {
      */
     @Override
     public void onReaderEvent(CardReaderEvent event) {
-
-      switch (event.getType()) {
-        case CARD_INSERTED:
-          pcscReader.transmitControlCommand(
-              pcscReader.getIoctlCcidEscapeCommandId(), CMD_SET_LED_RED);
-          break;
-        case CARD_MATCHED:
-          pcscReader.transmitControlCommand(
-              pcscReader.getIoctlCcidEscapeCommandId(), CMD_SET_LED_GREEN);
-          break;
-        case CARD_REMOVED:
-          pcscReader.transmitControlCommand(
-              pcscReader.getIoctlCcidEscapeCommandId(), CMD_SET_LED_BLUE);
-          break;
-        case UNAVAILABLE:
-          break;
+      try {
+        switch (event.getType()) {
+          case CARD_INSERTED:
+            pcscReader.transmitControlCommand(
+                pcscReader.getIoctlCcidEscapeCommandId(), CMD_BUZZER_200MS);
+            pcscReader.transmitControlCommand(
+                pcscReader.getIoctlCcidEscapeCommandId(), CMD_SET_LED_RED);
+            break;
+          case CARD_MATCHED:
+            pcscReader.transmitControlCommand(
+                pcscReader.getIoctlCcidEscapeCommandId(), CMD_BUZZER_200MS);
+            pcscReader.transmitControlCommand(
+                pcscReader.getIoctlCcidEscapeCommandId(), CMD_SET_LED_GREEN);
+            break;
+          case CARD_REMOVED:
+            pcscReader.transmitControlCommand(
+                pcscReader.getIoctlCcidEscapeCommandId(), CMD_SET_LED_BLUE);
+            break;
+          case UNAVAILABLE:
+            break;
+        }
+      } finally {
+        if (event.getType() != CardReaderEvent.Type.CARD_REMOVED) {
+          // indicates the end of the card processing
+          // (not needed for a removal event)
+          ((ObservableReader)
+                  SmartCardServiceProvider.getService()
+                      .getPlugins()
+                      .iterator()
+                      .next()
+                      .getReader(event.getReaderName()))
+              .finalizeCardProcessing();
+        }
       }
-      ((ObservableReader) (pcscReader)).finalizeCardProcessing();
     }
 
     @Override
