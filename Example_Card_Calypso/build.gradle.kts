701fbc504ad6cc7df6f750e79375a14ced2dfc1c
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2022-10-10 13:54:49 +0200

    feat: add Calypso SAM event counters and ceilings management example

--- Example_Card_Calypso/build.gradle.kts
+++ Example_Card_Calypso/build.gradle.kts
@@ -32,11 +32,13 @@ repositories {
 dependencies {
     implementation("org.calypsonet.terminal:calypsonet-terminal-reader-java-api:1.1.+") { isChanging = true }
     implementation("org.calypsonet.terminal:calypsonet-terminal-calypso-java-api:1.4.+") { isChanging = true }
+    implementation("org.calypsonet.terminal:calypsonet-terminal-calypso-crypto-legacysam-java-api:0.1.0-SNAPSHOT") { isChanging = true }
     implementation("org.eclipse.keyple:keyple-common-java-api:2.0.+") { isChanging = true }
     implementation("org.eclipse.keyple:keyple-service-java-lib:2.1.1")
     implementation("org.eclipse.keyple:keyple-service-resource-java-lib:2.0.2")
     implementation("org.eclipse.keyple:keyple-plugin-pcsc-java-lib:2.1.0")
     implementation("org.eclipse.keyple:keyple-plugin-stub-java-lib:2.1.0")
+    implementation("org.eclipse.keyple:keyple-card-calypso-crypto-legacysam-java-lib:0.1.0-SNAPSHOT") { isChanging = true }
     implementation("org.eclipse.keyple:keyple-card-calypso-java-lib:2.3.0")
     implementation("org.eclipse.keyple:keyple-util-java-lib:2.+") { isChanging = true }
     implementation("org.slf4j:slf4j-simple:1.7.32")
---
+++ Example_Card_Calypso/src/main/java/org/eclipse/keyple/card/calypso/example/UseCase14_SamEvent_Management/Main_SamEvent_Management_Pcsc.java
@@ -0,0 +1,95 @@
+/* **************************************************************************************
+ * Copyright (c) 2022 Calypso Networks Association https://calypsonet.org/
+ *
+ * See the NOTICE file(s) distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ************************************************************************************** */
+package org.eclipse.keyple.card.calypso.example.UseCase14_SamEvent_Management;
+
+import java.util.Map;
+import org.calypsonet.terminal.calypso.crypto.legacysam.sam.LegacySam;
+import org.calypsonet.terminal.calypso.crypto.legacysam.sam.LegacySamSelection;
+import org.calypsonet.terminal.calypso.crypto.legacysam.transaction.LegacySamFreeTransactionManager;
+import org.calypsonet.terminal.reader.CardReader;
+import org.calypsonet.terminal.reader.selection.CardSelectionManager;
+import org.calypsonet.terminal.reader.selection.CardSelectionResult;
+import org.eclipse.keyple.card.calypso.CalypsoExtensionService;
+import org.eclipse.keyple.card.calypso.crypto.legacysam.CardSelectionFactoryProvider;
+import org.eclipse.keyple.card.calypso.crypto.legacysam.LegacySamTransactionManagerFactoryProvider;
+import org.eclipse.keyple.card.calypso.example.common.ConfigurationUtil;
+import org.eclipse.keyple.core.service.Plugin;
+import org.eclipse.keyple.core.service.SmartCardService;
+import org.eclipse.keyple.core.service.SmartCardServiceProvider;
+import org.eclipse.keyple.core.util.json.JsonUtil;
+import org.eclipse.keyple.plugin.pcsc.PcscPluginFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main_SamEvent_Management_Pcsc {
+  private static final Logger logger = LoggerFactory.getLogger(Main_SamEvent_Management_Pcsc.class);
+
+  public static void main(String[] args) {
+    // Get the instance of the SmartCardService
+    SmartCardService smartCardService = SmartCardServiceProvider.getService();
+
+    // Register the PcscPlugin, get the corresponding PC/SC plugin in return
+    Plugin plugin = smartCardService.registerPlugin(PcscPluginFactoryBuilder.builder().build());
+
+    // Get the Calypso card extension service
+    CalypsoExtensionService calypsoCardService = CalypsoExtensionService.getInstance();
+
+    // Verify that the extension's API level is consistent with the current service.
+    smartCardService.checkCardExtension(calypsoCardService);
+
+    // Retrieve the SAM reader
+    CardReader samReader =
+        ConfigurationUtil.getSamReader(plugin, ConfigurationUtil.SAM_READER_NAME_REGEX);
+
+    // Create a SAM selection manager.
+    CardSelectionManager samSelectionManager =
+        SmartCardServiceProvider.getService().createCardSelectionManager();
+
+    // Create a SAM selection.
+    LegacySamSelection samSelection =
+        CardSelectionFactoryProvider.getFactory().createSamSelection();
+
+    // Provide the SAM selection to the card selection manager
+    samSelectionManager.prepareSelection(samSelection);
+
+    // SAM communication: run the selection scenario.
+    CardSelectionResult samSelectionResult =
+        samSelectionManager.processCardSelectionScenario(samReader);
+
+    // Check the selection result.
+    if (samSelectionResult.getActiveSmartCard() == null) {
+      throw new IllegalStateException("The selection of the SAM failed.");
+    }
+
+    // Get the Calypso legacy SAM SmartCard resulting of the selection.
+    LegacySam sam = (LegacySam) samSelectionResult.getActiveSmartCard();
+
+    logger.info("= SAM = {}", JsonUtil.toJson(sam));
+
+    LegacySamFreeTransactionManager samTransactionManager =
+        LegacySamTransactionManagerFactoryProvider.getFactory()
+            .createFreeTransactionManager(samReader, sam);
+
+    samTransactionManager.prepareReadEventCounters(0, 26);
+    samTransactionManager.prepareReadEventCeilings(0, 26);
+    samTransactionManager.processCommands();
+
+    for (Map.Entry<Integer, Integer> ceiling : sam.getEventCeilings().entrySet()) {
+      logger.info("Ceiling #{}: {}", ceiling.getKey(), ceiling.getValue());
+    }
+    for (Map.Entry<Integer, Integer> counter : sam.getEventCounters().entrySet()) {
+      logger.info("Counter #{}: {}", counter.getKey(), counter.getValue());
+    }
+
+    logger.info("= SAM = {}", JsonUtil.toJson(sam));
+  }
+}
